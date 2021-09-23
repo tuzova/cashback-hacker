929239b3f6bdd7b60fa472cdd13c9f5c1d43b876
Irine Tuzova <alexandrinius@outlook.com>
2021-09-23 16:05:37 +0300

    Build successful

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,9 +1,65 @@
 package ru.netology.service;
 
-import org.junit.jupiter.api.Test;
+import org.testng.annotations.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.testng.Assert.assertEquals;
 
-class CashbackHackServiceTest {
+// Напишите простые автотесты (без параметризации) на основании материала следующего раздела.
 
+
+public class CashbackHackServiceTest {
+    @Test
+    public void shouldCalculateRemain1() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 999;
+
+        int actual = service.remain(amount);
+        int expected = 1;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldCalculateRemain999() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1;
+
+        int actual = service.remain(amount);
+        int expected = 999;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldCalculateZeroRemain() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 1000;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldCalculateRemain1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 0;
+
+        int actual = service.remain(amount);
+        int expected = 1000;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldCalculateRemain750() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 2250;
+
+        int actual = service.remain(amount);
+        int expected = 750;
+
+        assertEquals(actual, expected);
+    }
 }
