c61d8edb2dbee695d9001fb92359eca7d61cb3d6
Irine Tuzova <alexandrinius@outlook.com>
2021-09-29 21:51:16 +0300

    Revision test Zero

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -33,7 +33,7 @@ public class CashbackHackServiceTest {
         int amount = 1000;
 
         int actual = service.remain(amount);
-        int expected = 1000;
+        int expected = 0;
 
         assertEquals(actual, expected);
     }
